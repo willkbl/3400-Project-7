b9a384e3425baa7f991ce6c335c8510f1035765e
Will Kibel <56790114+willkbl@users.noreply.github.com>
2024-11-04 22:54:39 -0500

    even better new mouselook

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -8,6 +8,10 @@ public class MouseLook : MonoBehaviour
     Transform playerBody;
     public float mouseSensitivity = 476;
 
+    float xAccumulator;
+    float yAccumulator;
+    const float Snappiness = 10.0f;
+
     float pitch = 0;
 
     // Start is called before the first frame update
@@ -25,13 +29,16 @@ public class MouseLook : MonoBehaviour
         //        if (!PauseMenuBehavior.isGamePaused)
         //        {
         float moveX = Input.GetAxis("Mouse X") * mouseSensitivity; //* Time.deltaTime;
+        xAccumulator = Mathf.Lerp(xAccumulator, moveX, Snappiness * Time.deltaTime);
         float moveY = Input.GetAxis("Mouse Y") * mouseSensitivity; //* Time.deltaTime;
+        yAccumulator = Mathf.Lerp(yAccumulator, moveY, Snappiness * Time.deltaTime);
 
-            //yaw
-            playerBody.Rotate(Vector3.up * moveX);
+
+        //yaw
+        playerBody.Rotate(Vector3.up * xAccumulator);
 
             //pitch
-            pitch -= moveY;
+            pitch -= yAccumulator;
 
             pitch = Mathf.Clamp(pitch, -90f, 90f);
             transform.localRotation = Quaternion.Euler(pitch, 0, 0);
